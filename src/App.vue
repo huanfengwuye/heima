2c82c7ab3dfd6a7ec6aa9185753d8a7d336e25e9
zsl <123@qq.com>
2020-02-19 20:17:52 +0800

    element-ui安装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    <el-butten>我是按钮</el-butten>
     <router-link to='#'>点我啊</router-link>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入element插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 
 // 导入router
 import router from './router/index'
